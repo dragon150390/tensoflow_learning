a7795ebc92cb2ebb9631462c4774a2e56e286d71
dragon150390 <jamebond.nguyen@gmail.com>
2018-10-30 13:04:38 +0700

    lession 3 own data for training

---
+++ tensor_less3.py
@@ -0,0 +1,14 @@
+import nltk
+# The first chanllenge when I will come up against after I learne based on the demos
+# preprogress the data We need to faced with that chanllenge
+
+#nltk.download()
+from nltk.tokenize import word_tokenize
+import numpy as np
+import random 
+import pickle 
+from collections import Counter
+from nltk.stem import WordNetLemmatizer
+
+lematizer = WordNetLemmatizer()
+hm_lines = 10000
